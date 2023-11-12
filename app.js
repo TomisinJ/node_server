000d75cc44a7d7d69a617f2b816bee7f45a5e38e
Tomisin <tomisinjoseph3@gmail.com>
2023-11-12 12:25:15 +0000

    rendered html to browser

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
  const http = require('http')
+ const fs = require('fs') // has all the file handling 
  const port = 3000
 
  const server = http.createServer(function(req, res) {
-    res.write('Hello Node')
-    res.end()
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File not found!') // write out text to the screen
+        } else {
+            res.write(data) // data parameter is all the info inside index.html
+        }
+        res.end()
+    })
  })
 
  server.listen(port, function(error) {
